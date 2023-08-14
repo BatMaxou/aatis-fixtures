e1142396536ddcb3153edfaeeddc583d944682a1
Batmaxou <maximebatista.lycee@gmail.com>
2023-08-14 10:43:28 +0200

    clean code

--- src/Command/GenerateDatafixturesCommand.php
+++ src/Command/GenerateDatafixturesCommand.php
@@ -2,7 +2,6 @@
 
 namespace App\Command;
 
-// use App\Service\Faker;
 use App\Service\DataGenerator;
 use Symfony\Component\Yaml\Yaml;
 use App\Exception\ConfigNotFoundException;
@@ -13,9 +12,9 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-    name: 'app:datafixtures:generate',
+    name: 'app:fixtures:generate',
 )]
-class GenerateDatafixturesCommand extends Command
+class GenerateFixturesCommand extends Command
 {
     private DataGenerator $generator;
 
@@ -28,42 +27,19 @@ class GenerateDatafixturesCommand extends Command
     protected function configure(): void
     {
         $this
-            ->setDescription('Generate datafixtures for the currents models');
+            ->setDescription('Generate fixtures for the currents models');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        /* FAKER TEST */
-
-        // $arrayTest = ['a', 'b', 'c'];
-
-        // var_dump(Faker::bool());
-        // var_dump(Faker::chooseBothFrom($arrayTest));
-        // var_dump(Faker::chooseKeyFrom($arrayTest));
-        // var_dump(Faker::chooseValueFrom($arrayTest));
-        // var_dump(Faker::company());
-        // var_dump(Faker::firstName());
-        // var_dump(Faker::float());
-        // var_dump(Faker::hexa());
-        // var_dump(Faker::int());
-        // var_dump(Faker::ipv4());
-        // var_dump(Faker::ipv6());
-        // var_dump(Faker::lastName());
-        // var_dump(Faker::oneOn(10));
-        // var_dump(Faker::repeatPatern('firstName', ' / ', 3));
-        // var_dump(Faker::stringInt());
-        // var_dump(Faker::word());
-        // var_dump(Faker::text(7));
-
-        /* FAKER TEST */
 
         $io = new SymfonyStyle($input, $output);
 
-        if (!$yaml = Yaml::parseFile('./datafixtures/config.yaml')) throw new ConfigNotFoundException('File "datafixtures/config.yaml" doesn\'t exist');
+        if (!$yaml = Yaml::parseFile('./fixtures/config.yaml')) throw new ConfigNotFoundException('File "fixtures/config.yaml" doesn\'t exist');
 
         $content = $this->generator->generate($yaml);
 
-        $path = './datafixtures/config.yaml';
+        $path = './fixtures/config.yaml';
         $dirname = dirname($path);
 
         if (!is_dir($dirname)) {
@@ -73,7 +49,7 @@ class GenerateDatafixturesCommand extends Command
         $file = fopen($path, 'w');
         fwrite($file, Yaml::dump($content));
 
-        $io->success('Succeded to create data for currents models into file ./datafixtures/config.yaml');
+        $io->success('Succeded to create data for currents models into file ./fixtures/config.yaml');
 
         return Command::SUCCESS;
     }
